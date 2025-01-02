618d0b943834dae0d0d276522aaf96b87c55d404
PK081982 <pkg081982@gmail.com>
2025-01-02 13:57:24 +0300

    first commit

---
+++ lesson-01/01.js
@@ -0,0 +1,11 @@
+// С помощью цикла while найдите сумму чисел от 1 до 20
+// Результат присвойте переменной sum
+let number=1;
+let num=0;
+while(number<=20)
+{
+num+=number;
+number++;
+}
+// your code
+let sum = num;
