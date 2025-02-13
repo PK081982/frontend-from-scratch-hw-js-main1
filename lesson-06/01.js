3ce31db257a367682db4e729ae7256ebc748e8ee
PK081982 <pkg081982@gmail.com>
2025-02-13 19:16:13 +0300

    js6

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -11,9 +11,32 @@
 
 🧙‍♂️ Совет: обратите внимание на управление индексом текущего изображения — это ключ к успешному переключению изображений.
 */
-
 const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
-]
+  ]
+  
+  var countNumber=WEB_TECH_IMAGES.length;
+  var numberElementArray=0;
+  
+  function myFunction(){
+        
+    numberElementArray--;
+        if(numberElementArray=== -1)
+      {
+        numberElementArray=countNumber-1;
+      }
+  
+        document.getElementById("web-tech-image").src= WEB_TECH_IMAGES[numberElementArray];
+      }
+  
+      function myFunction1(){
+        numberElementArray++;
+        if(numberElementArray=== countNumber)
+      {
+        numberElementArray=0;
+      }
+  
+        document.getElementById("web-tech-image").src= WEB_TECH_IMAGES[numberElementArray];
+      }
